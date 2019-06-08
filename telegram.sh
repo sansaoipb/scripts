bf21e2d17286299b80a08dd8c111a62df32ed4d0
sansaoipb <sansaoipb@gmail.com>
2019-06-07 22:10:13 -0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -29,13 +29,13 @@ then
   git clone $URLGIT
 fi
 
-cd $PROJETO ; sudo unzip telegram.zip ; sudo rm -rf README.md ; cd telegram ; sudo chmod +x telegram-cli ; cd ..
+cd $PROJETO ; sudo unzip telegram.zip ; sudo rm -rf README.md ; cd telegram ; sudo chmod +x telegram-cli* ; cd /tmp/
 
 if [ -e $DISTRO ]
 then
-  sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y python-requests.noarch openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel unzip git make ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
+  cd $PROJETO/telegram ; sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y python-requests.noarch openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel unzip git make ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
 else
-  sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 git unzip make
+  cd $PROJETO/telegram ; sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 git unzip make
 fi
 
 
@@ -46,9 +46,13 @@ else
   PATHSCRIPTS=/usr/local/share/zabbix/alertscripts
 fi
 
-sudo cp -R telegram* $PATHSCRIPTS ; cd $PATHSCRIPTS ; sudo chmod +x $PATHSCRIPTS/*.py ; cd .. ; sudo chown -R zabbix. *
+cd .. ; sudo cp -R telegram* $PATHSCRIPTS ; cd $PATHSCRIPTS ; sudo chmod +x $PATHSCRIPTS/*.py ; cd .. ; sudo chown -R zabbix. *
 
-replace 'config_directory = "/etc/zabbix/scripts/telegram/";' 'config_directory = "$PATHSCRIPTS/telegram/";' -- $PATHSCRIPTS/telegram/telegram.config
-
-cd $PATHSCRIPTS/telegram
-
+replace 'config_directory = "/etc/zabbix/scripts/telegram/";' 'config_directory = '$PATHSCRIPTS'/telegram/;' -- $PATHSCRIPTS/telegram/telegram.config
+echo ""
+echo "Acesse"
+echo ""
+echo cd $PATHSCRIPTS/telegram
+echo ""
+echo "para começar a configuração"
+echo ""
