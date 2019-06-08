b8cabc042cb9cb41785304f2db7e01451ae443c7
sansaoipb <sansaoipb@gmail.com>
2019-06-07 21:05:19 -0300

    Add files via upload

---
+++ telegram.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# ESCRITO POR SANSAO
+
+# Detecta se esta logado com usuario zabbix
+################################################################################
+#USER=$(whoami)
+#if [ "$USER" != "zabbix" ] ; then
+if [ `whoami` != "zabbix" ] ; then
+        echo ""
+        echo "voce deve estar logado com o user zabbix para continuar."
+        echo "Se nunca logou com ele, execute o comando em seguida logue"
+        echo ""
+        echo "sudo usermod -s /bin/bash zabbix ; sudo passwd zabbix"
+        echo ""
+		exit
+fi
+################################################################################
+
+SCRIPTS=/usr/lib/zabbix/alertscripts/
+
+# Detecta a DISTRO
+DISTRO=/etc/redhat-release
+PROJETO=Telegram-Graph-authenticated_Python
+URLGIT=https://github.com/sansaoipb/$PROJETO
+
+
+if [ ! -e $PROJETO ]
+then
+  git clone $URLGIT
+fi
+
+cd $PROJETO ; sudo unzip telegram.zip ; sudo rm -rf README.md ; cd telegram ; sudo chmod +x telegram-cli ; cd ..
+
+if [ -e $DISTRO ]
+then
+  sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y python-requests.noarch openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel unzip git make ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
+else
+  sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 git unzip make
+fi
+
+
+if [ -e $SCRIPTS ]
+then
+  PATHSCRIPTS=/usr/lib/zabbix/alertscripts
+else
+  PATHSCRIPTS=/usr/local/share/zabbix/alertscripts
+fi
+
+sudo cp -R telegram* $PATHSCRIPTS ; cd $PATHSCRIPTS ; sudo chmod +x $PATHSCRIPTS/*.py ; cd .. ; sudo chown -R zabbix. *
+
+replace 'config_directory = "/etc/zabbix/scripts/telegram/";' 'config_directory = "$PATHSCRIPTS/telegram/";' -- $PATHSCRIPTS/telegram/telegram.config
+
+cd $PATHSCRIPTS/telegram
+
