514ecca3f4274f0a6e00915336b443bfda035d3d
sansaoipb <sansaoipb@gmail.com>
2019-06-23 14:00:16 -0300

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -26,9 +26,9 @@ cd $PROJETO ; sudo unzip telegram.zip ; sudo rm -rf README.md ; sudo rm -rf tele
 
 if [ -e $DISTRO ]
 then
-  cd $PROJETO/telegram ; sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel python-pip ; pip install requests ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
+  cd $PROJETO/telegram ; sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel python-pip ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
 else
-  cd $PROJETO/telegram ; sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 python-pip ; pip install requests
+  cd $PROJETO/telegram ; sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 python-pip
 fi
 
 if [ -e $SCRIPTS ]
