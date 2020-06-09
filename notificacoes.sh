add16ead3c62621d7c82014b5c293b80661b77c7
sansaoipb <sansaoipb@gmail.com>
2020-06-09 19:33:43 -0300

    Add files via upload

--- notificacoes.sh
+++ notificacoes.sh
@@ -6,6 +6,13 @@ DISTRO=/etc/redhat-release
 PROJETO=Graphical_notifications_Zabbix
 URLGIT=https://github.com/sansaoipb/$PROJETO
 
+if [ -e $DISTRO ]
+then
+  sudo yum install -y jansson-devel openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel python3-devel python-pip python-pip3 python-requests python3-requests epel-release unzip git ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
+else
+  sudo apt-get install -y libjansson-dev libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 python-pip python-pip3 python-requests python3-requests unzip git
+fi
+
 if [ ! -e $PROJETO ]
 then
   git clone $URLGIT
@@ -15,9 +22,9 @@ cd $PROJETO ; sudo unzip telegram.zip ; sudo rm -rf README.md ; sudo rm -rf tele
 
 if [ -e $DISTRO ]
 then
-  cd $PROJETO/telegram ; sudo mv telegram-cli.CentOS telegram-cli ; sudo yum install -y jansson-devel openssl098e.x86_64 python34-libs libconfig-devel readline-devel libevent-devel lua-devel python-devel python3-devel python-pip python3-pip python-requests python3-requests ; sudo ln -s /usr/lib64/liblua-5.1.so /usr/lib64/liblua5.2.so.0 ; sudo ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.1.0.0
+  cd $PROJETO/telegram ; sudo mv telegram-cli.CentOS telegram-cli
 else
-  cd $PROJETO/telegram ; sudo rm -rf telegram-cli.CentOS ; sudo apt-get install -y libjansson-dev libreadline-dev libconfig-dev libssl-dev libevent-dev libjansson-dev libpython-dev libpython3-all-dev liblua5.2-0 python-pip python3-pip python-requests python3-requests
+  cd $PROJETO/telegram ; sudo rm -rf telegram-cli.CentOS
 fi
 
 if [ -e $SCRIPTS ]
@@ -35,7 +42,6 @@ then
 
 else
   cd /tmp/$PROJETO/ ; sudo cp -R telegram* notificacoes* $PATHSCRIPTS ; cd $PATHSCRIPTS ; sudo chmod +x $PATHSCRIPTS/*.py ; cd .. ; sudo chown -R zabbix. *
-
 fi
 
 sed -i "s,/etc/zabbix/scripts,$PATHSCRIPTS,g" $PATHSCRIPTS/telegram/telegram.config
@@ -47,7 +53,7 @@ clear
 echo ""
 echo "Execute"
 echo ""
-echo "cd '$PATHSCRIPTS'/telegram ; sudo -u zabbix ./telegram-cli --rsa-key tg-server.pub --config telegram.config"
+echo "cd $PATHSCRIPTS/telegram ; sudo -u zabbix ./telegram-cli --rsa-key tg-server.pub --config telegram.config"
 echo ""
 echo "para começar a configuração"
 echo ""
