914a07293d36c0027b5258fd716fe47ae362b4a9
sansaoipb <sansaoipb@gmail.com>
2024-10-07 12:16:49 -0300

    Update notificacoes2.sh

--- notificacoes2.sh
+++ notificacoes2.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # ESCRITO POR SANSÃO
 
-PROJETO=Graphical_notifications_Zabbix
-URLGIT=https://github.com/sansaoipb/$PROJETO
-PATHSCRIPTS0="/etc/zabbix/scripts"
-
 ############################################### WppConnect #############################################################
 # Atualizando o sistema e instalando dependências
 sudo apt update
@@ -14,21 +10,10 @@ sudo apt install nsolid -y
 #sudo apt install -y libgbm-dev unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libxss-dev libu2f-udev
 sudo apt install -y libgbm-dev unzip fontconfig locales libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libnss3 lsb-release xdg-utils libxss-dev libappindicator1 libu2f-udev
 
-
 # Instalando o Chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
-
-# Instalando e iniciando o WPP Connect
-cd ~
-git clone https://github.com/wppconnect-team/wppconnect-server.git
-cd wppconnect-server
-npm install
-npm run build
-sudo npm install -g pm2
-pm2 start npm --name wpp -- start
-
 
 # Configurando o NGINX
 sudo rm /etc/nginx/sites-enabled/default
@@ -58,12 +43,8 @@ sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo service ufw restart
 
-sed -i "s/deviceName: 'WppConnect'/deviceName: 'SendGraph'/" /root/wppconnect-server/src/config.ts
-
 sudo tee /usr/local/bin/wpp_connect > /dev/null <<EOF
-sudo su -
-cd /root/wppconnect-server
-sudo pm2 status
+cd $PATHSCRIPTS0/wppconnect-server
 pm2 start npm --name wpp -- start
 EOF
 
@@ -90,38 +71,8 @@ sudo systemctl enable wppconnect
 
 ############################################### WppConnect #############################################################
 
-mkdir -p $PATHSCRIPTS0
-
 apt-get install -y python3 python3-pip wget dos2unix git sudo curl bc vim; apt clean
 
 pythonVersion=$(/usr/bin/python3 -V 2>&1 | cut -d' ' -f2 | cut -d. -f1,2)
 rm -rf /usr/lib/python$pythonVersion/EXTERNALLY-MANAGED
 cd /tmp/ ; sudo /usr/bin/pip3 install wheel requests urllib3 pyrogram tgcrypto pycryptodome flask
-
-git clone -b beta $URLGIT
-
-if [ -z "$PROJETO" ] ; then
-  echo "Script não mexecutado, analise para resolver."
-  exit 1
-fi
-
-sleep 5
-
-if [ ! -e "$PATHSCRIPTS0/configScripts.properties" ] ; then
-   sudo cp -R /tmp/$PROJETO/configScripts.properties $PATHSCRIPTS0
-fi
-
-cd /tmp/$PROJETO/
-sudo cp -R notificacoes* $PATHSCRIPTS0
-cd $PATHSCRIPTS0
-sudo chmod +x *.py
-dos2unix *.py
-sudo rm -rf /tmp/$PROJETO/
-
-echo ""
-echo "Execute o comando abaixo para editar o arquivo de configuração:"
-echo ""
-echo "cd $PATHSCRIPTS0 ; sudo -u zabbix vim configScripts.properties"
-echo ""
-echo "e vamos começar com os envios"
-echo ""
