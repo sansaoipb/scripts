c73674cac6437c03d37fe4f77ec16fae5ba1f211
sansaoipb <sansaoipb@gmail.com>
2024-10-08 00:13:18 -0300

    Create notificacoes_3.sh

---
+++ notificacoes_3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ESCRITO POR SANSÃO
+
+PATHSCRIPTS="/etc/zabbix/scripts"
+PROJETO=Graphical_notifications_Zabbix
+URLGIT=https://github.com/sansaoipb/$PROJETO
+
+apt-get install -y python3 python3-pip wget dos2unix git sudo curl bc vim; apt clean
+
+pythonVersion=$(/usr/bin/python3 -V 2>&1 | cut -d' ' -f2 | cut -d. -f1,2)
+rm -rf /usr/lib/python$pythonVersion/EXTERNALLY-MANAGED
+cd /tmp/ ; sudo /usr/bin/pip3 install wheel requests urllib3 pyrogram tgcrypto pycryptodome flask
+
+if [ ! -e $PROJETO ] ; then
+  git clone -b beta $URLGIT
+fi
+
+if [ ! -e "$PATHSCRIPTS/configScripts.properties" ] ; then
+  sudo cp -R /tmp/$PROJETO/configScripts.properties $PATHSCRIPTS
+fi
+
+sudo cp -R /tmp/$PROJETO/notificacoes* $PATHSCRIPTS ; sudo chmod +x $PATHSCRIPTS/*.py ; dos2unix $PATHSCRIPTS/*.py ; sudo rm -rf /tmp/$PROJETO
+
+echo ""
+echo "Execute o comando abaixo para editar o arquivo de configuração:"
+echo ""
+echo "cd $PATHSCRIPTS ; sudo -u zabbix vim configScripts.properties"
+echo ""
+echo "e vamos começar com os envios"
+echo ""
+
+sudo rm -rf /tmp/notificacoes.sh
