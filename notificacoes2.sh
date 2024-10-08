1c74726cd1a4694636680db6c0bd2d19967adcb7
sansaoipb <sansaoipb@gmail.com>
2024-10-07 21:03:55 -0300

    Update notificacoes2.sh

--- notificacoes2.sh
+++ notificacoes2.sh
@@ -12,14 +12,12 @@ sudo apt-get install nodejs -y
 #sudo apt install -y libgbm-dev unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libxss-dev libu2f-udev
 sudo apt install -y libgbm-dev unzip fontconfig locales libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libnss3 lsb-release xdg-utils libxss-dev libappindicator1 libu2f-udev
 
+sudo mkdir -p $PATHSCRIPTS
+
 # Instalando o Chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
-
-if [ -z $PATHSCRIPTS ]; then
-  sudo mkdir -p $PATHSCRIPTS
-fi
 
 cd $PATHSCRIPTS
 git clone https://github.com/wppconnect-team/wppconnect-server.git
