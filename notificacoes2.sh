b44a0c2c0641a7da9ac0ec46f3d4d1575743fd80
sansaoipb <sansaoipb@gmail.com>
2024-10-07 22:08:16 -0300

    Update notificacoes2.sh

--- notificacoes2.sh
+++ notificacoes2.sh
@@ -18,7 +18,7 @@ sudo mkdir -p $PATHSCRIPTS
 
 # Instalando o Chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb ; rm -rf google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
 
 cd $PATHSCRIPTS
